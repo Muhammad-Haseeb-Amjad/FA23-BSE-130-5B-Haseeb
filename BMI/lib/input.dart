e2c3cef43269e46b0b626db54a603427b945fe12
Muhammad-Haseeb-Amjad <muhammadhaseebamjad24@gmail.com>
2025-11-26 18:38:06 +0500

    video 7
    Use GestureDetector

--- BMI/lib/input.dart
+++ BMI/lib/input.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'Icontextfile.dart';
 import 'Containerfile.dart';
+const activeColor = Color(0xFF1D1E33);
+const deActiveColor = Color(0xFF111328);
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
@@ -13,58 +15,75 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  Color maleColor = deActiveColor;
+  Color femaleColor = deActiveColor;
+
+  void updateColor(int gender){
+    if(gender==1){
+      maleColor = activeColor;
+      femaleColor = deActiveColor;
+    }
+    if(gender==2){
+      femaleColor = activeColor;
+      maleColor = deActiveColor;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme
-            .of(context)
-            .colorScheme
-            .inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Column(
-          children: [
-      Expanded(child: Row(
-      children: [
-          Expanded(
-            child: new repeatcontainer(
-                colors: Color(0xFF1D1E33),
-        cardWidget: repeatIcon(
-          icondata: FontAwesomeIcons.male,
-          label: 'Male',
+        appBar: AppBar(
+          backgroundColor: Theme
+              .of(context)
+              .colorScheme
+              .inversePrimary,
+          title: Text(widget.title),
         ),
-            ),
-          ),
-    Expanded(child: new repeatcontainer(
-        colors: Color(0xFF1D1E33),
-    cardWidget: repeatIcon(
-      icondata: FontAwesomeIcons.female,
-      label: 'Female',
-    ),
-    ),),
-    ],
-    ),),
-    Expanded(child: new repeatcontainer(colors: Color(0xFF1D1E33)),),
-    Expanded(child: Row(
-    children: [
-    Expanded(child: new repeatcontainer(colors: Color(0xFF1D1E33)),),
-    Expanded(child: new repeatcontainer(colors: Color(0xFF1D1E33)),),
-    ],
-    ),),
-    ],
-    )
-    // This trailing comma makes auto-formatting nicer for build methods.
+        body: Column(
+            children: [
+              Expanded(child: Row(
+                children: [
+                  Expanded(
+                    child: GestureDetector(
+                      onTap: (){
+                        setState(() {
+                          updateColor(1);
+                        });
+                      },
+                      child:  repeatcontainer(
+                        colors: maleColor,
+                        cardWidget: repeatIcon(
+                          icondata: FontAwesomeIcons.male,
+                          label: 'Male',
+                        ),
+                      ),
+                    ),
+                  ),
+                  Expanded(child: GestureDetector(
+                    onTap: (){
+                      setState(() {
+                        updateColor(2);
+                      });
+                    },
+                    child: repeatcontainer(
+                      colors: femaleColor,
+                      cardWidget: repeatIcon(
+                        icondata: FontAwesomeIcons.female,
+                        label: 'Female',
+                      ),
+                    ),
+                  ),),
+                ],
+              ),),
+              Expanded(child:  repeatcontainer(colors: Color(0xFF1D1E33)),),
+              Expanded(child: Row(
+                children: [
+                  Expanded(child: repeatcontainer(colors: Color(0xFF1D1E33)),),
+                  Expanded(child:  repeatcontainer(colors: Color(0xFF1D1E33)),),
+                ],
+              ),),
+            ]
+        )
     );
   }
 }
-
-
-
-
