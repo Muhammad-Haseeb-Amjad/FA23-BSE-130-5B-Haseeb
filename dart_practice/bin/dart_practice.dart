eba7de229fa2332ae5f822d6c0cd1d305e0f2acf
Muhammad-Haseeb-Amjad <muhammadhaseebamjad24@gmail.com>
2026-05-07 19:28:46 +0500

    Add dart_practice project

---
+++ dart_practice/bin/dart_practice.dart
@@ -0,0 +1,32 @@
+void main() {
+  var grade = "B";
+  switch (grade){
+    case"A":
+      print("Excellent");
+      break;
+    case"B":
+      print("Good");
+    default:
+      print("Try Again");
+  }
+
+}
+
+
+
+
+// void main() {
+//   int score = 80;
+//   if (score > 50) {
+//     print("Passed");
+//   }
+//   else {
+//     print("Failed");
+//   }
+// }
+
+
+
+// void main(){
+//   print("M.Haseeb Amjad");
+// }
